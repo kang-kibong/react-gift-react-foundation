4f57ac9a1f7fcb4ed94458b51234962b23fa123a
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-06-28 20:16:03 +0900

    feat: add GoodsItem component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Global } from '@emotion/react';
 import resetStyles from '@styles/resetStyles';
 import Button from '@components/Button/Button';
 import Input from '@components/Input/Input';
-import Image from '@components/Image';
+// import Image from '@components/Image';
+import GoodsItem from '@components/GoodsItem/GoodsItem';
 
 function App() {
   return (
@@ -14,11 +15,17 @@ function App() {
         hello World
       </Button>
       <Input disabled invalid size="responsive" />
-      <Image
+      {/* <Image
         src="https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png"
         ratio={16 / 9}
         width={300}
         height={300}
+      /> */}
+      <GoodsItem
+        imageSrc="https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png"
+        subtitle="카카오 프렌즈 특별 한정판"
+        title="[특가] 카카오 프렌즈 특별 한정판 브라이트 쿠션"
+        amount={100000000000}
       />
     </>
   );
--- src/components/GoodsItem/GoodsItem.tsx
+++ src/components/GoodsItem/GoodsItem.tsx
@@ -1,25 +1,64 @@
-// import styled from '@emotion/styled';
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { ReactNode } from 'react';
+import Image from '@components/Image';
 
 export interface GoodsItemProps {
   imageSrc: string;
   subtitle: string;
   title: string;
   amount: number;
+  children: ReactNode;
 }
 
-export default function GoodsItem({ imageSrc, subtitle, title, amount }: GoodsItemProps) {
+export default function GoodsItem({ imageSrc, subtitle, title, amount, children, ...props }: GoodsItemProps) {
   return (
-    <div>
-      <img src={imageSrc} alt={title} />
-      <div>
-        <p>{subtitle}</p>
-        <h3>{title}</h3>
-        <p>
+    <StyledGoodsItem {...props}>
+      {children}
+      <Image src={imageSrc} width={120} height={120} radius={4} />
+      <Content>
+        <SubTitle>{subtitle}</SubTitle>
+        <Title>{title}</Title>
+        <Amount>
           {amount}
           <span>원</span>
-        </p>
-      </div>
-    </div>
+        </Amount>
+      </Content>
+    </StyledGoodsItem>
   );
 }
+
+const StyledGoodsItem = styled.div`
+  width: 120px;
+`;
+
+const Content = styled.div`
+  padding-top: 12px;
+`;
+
+const SubTitle = styled.p`
+  color: #999999;
+  font-size: 14px;
+  margin-bottom: 4px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Amount = styled.p`
+  font-size: 20px;
+  font-weight: 700;
+  padding-top: 8px;
+  word-break: break-word;
+
+  & > span {
+    font-weight: 400;
+  }
+`;
